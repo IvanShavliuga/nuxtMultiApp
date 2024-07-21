a02d63dc9707bce3cf8c99f7715f8d094d1048d5
Ivan Shavliuga (Ivanov) <38425718+IvanShavliuga@users.noreply.github.com>
2024-07-21 11:21:54 +0300

    created header component

--- app.vue
+++ app.vue
@@ -1,10 +1,9 @@
 <template>
   <div>
+    <BaseHeader/>
     <NuxtPage />
   </div>
 </template>
 <script setup lang="ts">
-const test = 2
-console.log('t')
 </script>
 
---
+++ components/BaseHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <ul>
+        <li v-for="link in listMenu" :key="link.href">
+            <a :href="link.href">{{ link.label }}</a>
+        </li> 
+    </ul>
+  </header>
+</template>
+
+<script setup lang="ts">
+//import { defineProps, ref, toRefs, PropType } from 'vue'
+import type { ListLinks } from '@/types/Lists'
+const listMenu = ref<ListLinks>([
+    { label: 'главная', href: '/'},
+    { label: 'группы', href: '/groups'},
+    { label: 'пользователи', href: '/users'}
+])
+</script>
+
+<style>
+
+</style>
