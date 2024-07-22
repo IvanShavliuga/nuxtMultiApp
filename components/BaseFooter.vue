425d2238178589caff0f47d215a678c81681c999
Ivan Shavliuga (Ivanov) <38425718+IvanShavliuga@users.noreply.github.com>
2024-07-22 15:17:52 +0300

    created component footer

---
+++ components/BaseFooter.vue
@@ -0,0 +1,37 @@
+<template>
+  <footer class="footer">
+    <a class="footer__link" href="https://ivanshavliuga.github.io/" target="_blank">Ivan Shavliuga</a>
+    <a  class="footer__link" href="https://github.com/IvanShavliuga/nuxtMultiApp" target="_blank">source code</a>
+    <p class="footer__copyright">(C) 2024</p> 
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="less">
+.footer {
+    display: flex;
+    justify-content: space-between;
+    gap: 8px;
+    background-color: black;
+    color: white;
+    font-size: 14px;
+    padding: 8px 16px;
+    margin-top: 16px;
+    &__link {
+        color: purple;
+        text-decoration: none;
+        &:first-child {
+            border-right: 1px solid white;
+            padding-right: 8px;
+        }
+    }
+    &__copyright {
+        margin-left: auto;
+    }
+}
+</style>
