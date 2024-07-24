91fcf3d7fa1b3dc1bc904ec98752974c027a06dc
Ivan Shavliuga (Ivanov) <38425718+IvanShavliuga@users.noreply.github.com>
2024-07-24 10:45:51 +0300

    correct mapping news server api

--- server/api/news.js
+++ server/api/news.js
@@ -5,18 +5,18 @@ export default eventHandler(async (event) => {
     const filePath = path.join(process.cwd(), 'public', 'db.json')
     const data = JSON.parse(await fs.promises.readFile(filePath, 'utf-8'))
     const { users, comments, groups } = data
-    console.log(groups)
     const posts = data.posts.map((p) => {
         const usr = users.filter((u) => u.id === p.userId)
         const fm = {...p, user: {}, group: {}}
+        const user = {
+            avatar: usr[0].avatar,
+            name: usr[0].name,
+            spec: usr[0].spec, 
+            skills: usr[0].skills, 
+            city: usr[0].city,
+        }
         if (usr.length) {
-            fm.user = {
-                avatar: usr[0].avatar,
-                name: usr[0].name,
-                spec: usr[0].spec, 
-                skills: usr[0].skills, 
-                city: usr[0].city,
-            }
+            fm.user = user
         }
         const grp = groups.filter((g) => g.id === p.groupId)
         if (grp.length) {
@@ -26,7 +26,7 @@ export default eventHandler(async (event) => {
                 isAdmin: grp[0].isAdmin === p.userId
             }
         }
-        fm.comments = comments.filter((c) => c.postId === p.id)
+        fm.comments = comments.filter((c) => c.postId === p.id).map((c) => ({ ...c, user}))
         fm.like = users.filter((u) => p.like.includes(u.id)).map((u) => ({ avatar: u.avatar, id: u.id, name: u.name }))
         fm.repost = users.filter((u) => p.repost.includes(u.id)).map((u) => ({ avatar: u.avatar, id: u.id, name: u.name }))
         fm.views = users.filter((u) => p.views.includes(u.id)).map((u) => ({ avatar: u.avatar, id: u.id, name: u.name }))
