b3a2ee3c9fb2de9f3fa1d48fa40628fdfe8301c2
Ivan Shavliuga (Ivanov) <38425718+IvanShavliuga@users.noreply.github.com>
2024-07-29 14:34:17 +0300

    fixed arrow for tooltip

--- components/UserTooltip.vue
+++ components/UserTooltip.vue
@@ -15,13 +15,26 @@ const props = defineProps({  name: String, spec: String, city: String})
 <style scoped lang="less">
 .userTooltip {
     position: absolute;
-    top: -70px;
+    top: -60px;
     left: -90px;
     background-color: white;
     min-width: 180px;
-    padding: 16px;
+    padding: 8px;
     border-radius: 10px;
     border: 1px solid silver;
+    &::after {
+      position: absolute;
+      bottom: -9px;
+      right: 22px;
+      display: block;
+      content: ' ';
+      background-color: white;
+      border: solid silver;
+      border-width: 0 1px 1px 0;
+      padding: 8px;
+      transform: rotate(45deg);
+      -webkit-transform: rotate(45deg);
+    }
     &__name {
         font-size: 14px;
     }
