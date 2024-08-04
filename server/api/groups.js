c96ff830d7a8785249c00de816c91cfab3b4985d
Ivan Shavliuga (Ivanov) <38425718+IvanShavliuga@users.noreply.github.com>
2024-08-04 15:45:36 +0300

    created api query get group by adminId & followId

--- server/api/groups.js
+++ server/api/groups.js
@@ -1,7 +1,7 @@
 // /server/api/read.get.ts
 import * as path from "node:path";
 import * as fs from "node:fs";
-export default eventHandler(async () => {
+export default eventHandler(async (event) => {
   //event
   const filePath = path.join(process.cwd(), "public", "db.json");
   const data = JSON.parse(await fs.promises.readFile(filePath, "utf-8"));
@@ -31,6 +31,32 @@ export default eventHandler(async () => {
     r.posts = posts.filter((p) => g.idNews.includes(p.groupId));
     return r;
   });
+  const { url } = event.node.req;
+  const query = url.split("?");
+  if (query.length === 2) {
+    const parametrs = query[1].split("&").map((p) => {
+      const pd = p.split("=");
+      const po = {};
+      po[pd[0]] = pd[1];
+      return po;
+    });
+    const filterRes = [];
+    const adminParametr = parametrs.filter((p) => "admin" in p);
+    if (adminParametr.length) {
+      const grp = res.filter((u) => u.idAdmin === +adminParametr[0].admin);
+      filterRes.push(...grp);
+    }
+    const followParametr = parametrs.filter((p) => "follow" in p);
+    if (followParametr.length) {
+      const grp = res.filter((u) =>
+        u.followers.findIndex((f) => f.id === +followParametr[0].follow) > -1,
+      );
+      filterRes.push(...grp);
+    }
+    return {
+      groups: filterRes,
+    };
+  }
   return {
     groups: res,
   };
