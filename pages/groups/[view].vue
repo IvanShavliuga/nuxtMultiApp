e05e6c17c38f3cafc4bfe80bfcddccc8875848a6
Ivan Shavliuga (Ivanov) <38425718+IvanShavliuga@users.noreply.github.com>
2024-08-07 13:46:22 +0300

    test base markup for group detail page

--- pages/groups/[view].vue
+++ pages/groups/[view].vue
@@ -1,6 +1,55 @@
 <template>
-  <main class="wrapper">
-    <h1 class="headerPage">Группа {{ viewId }}</h1>
+  <main class="groupPage wrapper">
+    <div class="groupPanel">
+      <h2 class="groupPage__header">{{ groupView.name }}</h2>
+      <p class="groupPanel__desc">{{ groupView.category }}</p>
+      <p class="groupPanel__desc">{{ groupView.desc }}</p>
+      <!-- <ul class="groupPanel__list">
+          <li v-for="f in getUserAllData.userView.friends" :key="f.id">
+            <UserShortCard
+              :avatar="f.avatar"
+              :login="f.login"
+              :name="f.name"
+              :spec="f.spec"
+            />
+          </li>
+        </ul> -->
+      <div class="groupStat">
+        <p class="groupStat__text">
+          Количество постов:
+          <span class="groupStat__value"> 0</span>
+        </p>
+        <p class="groupStat__text">
+          Количество лайков:
+          <span class="groupStat__value"> 0</span>
+        </p>
+        <p class="groupStat__text">
+          Количество репостов:
+          <span class="groupStat__value"> 0</span>
+        </p>
+        <p class="groupStat__text">
+          Количество комментариев:
+          <span class="groupStat__value"> 0</span>
+        </p>
+        <p class="groupStat__text">
+          Количество просмотров:
+          <span class="groupStat__value"> 0</span>
+        </p>
+      </div>
+      <div class="groupPanel__flexblock">
+        <div>
+          <p class="groupPanel__text">
+            Подписчиков:<span class="groupPanel__value">
+              {{ groupView.followers.length }}</span
+            >
+          </p>
+          <AvaList :list="avatars" />
+        </div>
+        <div class="groupPanel__admin">
+          <UserBox title="Админ" :user="groupView.admin" />
+        </div>
+      </div>
+    </div>
     <pre>
     {{ getGroupsAllData }}
    </pre
@@ -17,7 +66,9 @@ const { currentRoute } = useRouter();
 const viewId: string = currentRoute.value.params.view as string;
 const load = ref(false);
 await addGroupView(+viewId);
+const { groupView } = getGroupsAllData;
 load.value = true;
+const avatars = groupView.followers.map((f) => f.avatar);
 </script>
 
 <style scoped lang="less">
@@ -25,8 +76,69 @@ load.value = true;
 .wrapper {
   .pageWrapper();
 }
-.headerPage {
-  color: @headerPageColor;
-  font-size: @headerPageSize;
+.groupPage {
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  flex-wrap: wrap;
+  gap: 16px;
+  &__header {
+    color: @blockHeaderColor;
+    font-size: 18px;
+  }
+}
+.groupPanel {
+  display: flex;
+  flex-direction: column;
+  border: 1px solid @blockBorder;
+  border-radius: 16px;
+  padding: @blockPadding;
+  background-color: @blockColor;
+  margin: 16px 0;
+  width: 400px;
+  &__flexblock {
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+    flex-wrap: wrap;
+    gap: 16px;
+    padding: 8px 0;
+    border-top: 1px solid @blockBorder;
+  }
+  &__list {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 8px;
+    list-style: none;
+    margin: 16px 0;
+  }
+  &__desc {
+    color: @blockTextColor;
+    font-size: 14px;
+  }
+  &__admin {
+    width: 20ch;
+  }
+  &__text {
+    color: @blockTextColor;
+  }
+  &__value {
+    color: @blockValueColor;
+    font-weight: 700;
+  }
+}
+.groupStat {
+  margin: 8px 0;
+  padding: 8px 0;
+  border-top: 1px solid @blockBorder;
+  &__text {
+    color: @blockTextColor;
+  }
+  &__value {
+    color: @blockValueColor;
+    font-weight: 700;
+  }
 }
 </style>
